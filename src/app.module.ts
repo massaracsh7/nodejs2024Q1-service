56fe6294bcce7cb46b80b742826a0ced0bafc7b2
massaracsh7 <fatum7@gmail.com>
2024-03-07 22:13:45 +0300

    feat: add favorites module service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { UserModule } from './users/user.module';
 import { ArtistModule } from './artists/artist.module';
 import { TrackModule } from './tracks/track.module';
 import { AlbumModule } from './albums/album.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [UserModule, ArtistModule, TrackModule, AlbumModule],
+  imports: [
+    UserModule,
+    ArtistModule,
+    TrackModule,
+    AlbumModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
