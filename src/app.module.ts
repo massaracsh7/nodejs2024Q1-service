8b0f6016d577ff90d74720da18ccde18dc9287a3
massaracsh7 <fatum7@gmail.com>
2024-03-07 20:41:29 +0300

    feat: add artist module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
+import { ArtistModule } from './artists/artist.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
