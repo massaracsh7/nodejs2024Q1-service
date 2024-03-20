d3b34a72db1b6326e92c250ca8e8f4f7c122325a
massaracsh7 <fatum7@gmail.com>
2024-03-20 20:45:55 +0300

    refactor: update album prisma

--- src/albums/album.service.ts
+++ src/albums/album.service.ts
@@ -3,18 +3,22 @@ import { v4 as uuidv4 } from 'uuid';
 import { CreateAlbumDto, UpdateAlbumDto } from './dto/album.dto';
 import { Album } from '../types';
 import { FavoritesService } from 'src/favorites/favorites.service';
-import { Data } from 'src/data/data.service';
+//import { Data } from 'src/data/data.service';
+import { Prisma } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AlbumService {
-  constructor(private data: Data, private favoriteService: FavoritesService) {}
+  constructor(
+    private prisma: Prisma,
+    private favoriteService: FavoritesService,
+  ) {}
 
   async findAll(): Promise<Album[]> {
-    return this.data.albums;
+    return this.prisma.albums;
   }
 
   async findOne(id: string): Promise<Album> {
-    const album = this.data.albums.find((album) => album.id === id);
+    const album = this.prisma.albums.find((album) => album.id === id);
     if (!album) {
       throw new NotFoundException('Album is not found');
     }
@@ -26,33 +30,33 @@ export class AlbumService {
       id: uuidv4(),
       ...createAlbumDto,
     };
-    this.data.albums.push(newAlbum);
+    this.prisma.albums.push(newAlbum);
     return newAlbum;
   }
 
   async update(id: string, updateAlbumDto: UpdateAlbumDto): Promise<Album> {
-    const i = this.data.albums.findIndex((item) => item.id === id);
+    const i = this.prisma.albums.findIndex((item) => item.id === id);
     if (i === -1) {
       throw new NotFoundException('Album is not found');
     }
-    this.data.albums[i] = {
-      ...this.data.albums[i],
+    this.prisma.albums[i] = {
+      ...this.prisma.albums[i],
       ...updateAlbumDto,
     };
-    return this.data.albums[i];
+    return this.prisma.albums[i];
   }
 
   async remove(id: string): Promise<boolean> {
-    const i = this.data.albums.findIndex((item) => item.id === id);
+    const i = this.prisma.albums.findIndex((item) => item.id === id);
     if (i === -1) {
       throw new NotFoundException('Album is not found');
     }
-    this.data.albums.splice(i, 1);
-    const iFav = this.data.favorites.albums.findIndex((item) => item === id);
+    this.prisma.albums.splice(i, 1);
+    const iFav = this.prisma.favorites.albums.findIndex((item) => item === id);
     if (iFav !== -1) {
       this.favoriteService.removeAlbum(id);
     }
-    this.data.tracks = this.data.tracks.map((item) => {
+    this.prisma.tracks = this.prisma.tracks.map((item) => {
       if (item.albumId === id) {
         return {
           ...item,
