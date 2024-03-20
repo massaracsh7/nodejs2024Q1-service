746d3f7bf0a659edbfffef196fd0b097640edfc0
massaracsh7 <fatum7@gmail.com>
2024-03-20 20:50:20 +0300

    refactor: update track prisma

--- src/tracks/track.service.ts
+++ src/tracks/track.service.ts
@@ -3,18 +3,22 @@ import { Track } from '../types';
 import { CreateTrackDto, UpdateTrackDto } from './dto/track.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { FavoritesService } from 'src/favorites/favorites.service';
-import { Data } from 'src/data/data.service';
+//import { Data } from 'src/data/data.service';
+import { Prisma } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TrackService {
-  constructor(private data: Data, private favoriteService: FavoritesService) {}
+  constructor(
+    private prisma: Prisma,
+    private favoriteService: FavoritesService,
+  ) {}
 
   async findAll(): Promise<Track[]> {
-    return this.data.tracks;
+    return this.prisma.tracks;
   }
 
   async findOne(id: string): Promise<Track> {
-    const track = this.data.tracks.find((track) => track.id === id);
+    const track = this.prisma.tracks.find((track) => track.id === id);
     if (!track) {
       throw new NotFoundException('Track is not found');
     }
@@ -26,29 +30,29 @@ export class TrackService {
       id: uuidv4(),
       ...createTrackDto,
     };
-    this.data.tracks.push(newTrack);
+    this.prisma.tracks.push(newTrack);
     return newTrack;
   }
 
   async update(id: string, updateTrackDto: UpdateTrackDto): Promise<Track> {
-    const i = this.data.tracks.findIndex((item) => item.id === id);
+    const i = this.prisma.tracks.findIndex((item) => item.id === id);
     if (i === -1) {
       throw new NotFoundException('Track is not found');
     }
-    this.data.tracks[i] = {
-      ...this.data.tracks[i],
+    this.prisma.tracks[i] = {
+      ...this.prisma.tracks[i],
       ...updateTrackDto,
     };
-    return this.data.tracks[i];
+    return this.prisma.tracks[i];
   }
 
   async remove(id: string): Promise<boolean> {
-    const i = this.data.tracks.findIndex((item) => item.id === id);
+    const i = this.prisma.tracks.findIndex((item) => item.id === id);
     if (i === -1) {
       throw new NotFoundException('Track is not found');
     }
-    this.data.tracks.splice(i, 1);
-    const iFav = this.data.favorites.tracks.findIndex((item) => item === id);
+    this.prisma.tracks.splice(i, 1);
+    const iFav = this.prisma.favorites.tracks.findIndex((item) => item === id);
     if (iFav !== -1) {
       this.favoriteService.removeTrack(id);
     }
